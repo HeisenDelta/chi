f5f554d88c01706a436c97a3e030cd99fd68c6f0
HeisenDelta <66578560+HeisenDelta@users.noreply.github.com>
2022-07-09 18:39:20 +0900

    Commence time test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import json
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-file = open('info.json', 'r')
-info = json.load(file)
+info_file = open('info.json', 'r')
+info = json.load(info_file)
 
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
@@ -11,8 +11,14 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
+    
+    time_file = open('time.text', 'r')
+    time_ = time_file.readline()
 
-    messages = TextSendMessage(text = 'The time is now 5:50 PM')
+    messages = TextSendMessage(text = f'The time is now {time_[0]}:{time_[1:]} PM')
+    
+    time_file.write(str(int(time_) + 10))
+    time_file.close()
     line_bot_api.push_message(USER_ID, messages = messages)
 
 
