155753ae1e1eeb205c5564961b2e761c8461cef5
Ariya _ <heisen@definitely-not-windows.local>
2022-07-11 17:44:34 +0900

    Reverted tests for time checking

--- main.py
+++ main.py
@@ -11,15 +11,8 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    
-    time_file = open('time.txt', 'r')
-    time_ = time_file.readline()
 
-    messages = TextSendMessage(text = f'The time is now {time_[0]}:{time_[1:]} PM')
-    
-    time_file.write(str(int(time_) + 10))
-    time_file.close()
+    messages = TextSendMessage(text = f'Testing')
     line_bot_api.push_message(USER_ID, messages = messages)
-
 
 if __name__ == '__main__': main()
